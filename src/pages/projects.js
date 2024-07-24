da5b9e5118b0112d20e38265ad04e1ecbcb96c9f
NurFer13 <nurfer1307@gmail.com>
2024-07-24 11:09:01 +0000

    changing projects display

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,20 +13,12 @@ export default function Projects (){
 <>
 <NavBar/>
 <h1 className="projecth1">PROJECTS</h1>
+
 <div className="proyectitos ">
-<div className="row"></div>
-<div class="col">
 
-<div className="card"><Link className="projectCard" to="../projectcard">
-  <img src={project1} className="card-image1" alt="..."/>
-  <div className="card-body">
-    <h2 className="card-title">WHAT'S BEHIND?</h2>
-    <p className="card-text">Competititon Festival Model - Temple Barcelona</p>
-  </div></Link>
-  </div>
-</div>
-<div class="col">
+<div className="row">
 
+<div class="col">
 <div className="card"><Link className="projectCard" to="../projectcard">
   <img src={project2} className="card-image2" alt="..."/>
   <div className="card-body">
@@ -44,35 +36,42 @@ export default function Projects (){
   </div></Link>
 </div>
 </div>
+</div>
 
-
+<div className="row">
+<div className="col">
 <div className="card"><Link className="projectCard" to="../projectcard">
   <img src={project4} className="card-image4" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CHIESA DIRUTA</h2>
     <p className="card-text">Competition Reuse Italy -Concert Hall - Chiesa Diruta</p>
   </div></Link>
-</div>
+</div></div>
+<div className="col">
 <div className="card"><Link className="projectCard" to="../projectcard">
   <img src={project5} className="card-image5" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CASA GV506</h2>
     <p className="card-text">Private client - Integral reform - Barcelona</p>
   </div></Link>
-</div>
+</div></div>
+<div className="col">
 <div className="card"><Link className="projectCard" to="../projectcard">
   <img src={project6} className="card-image6" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CENTRO FMG</h2>
     <p className="card-text">Private client - New Construction - Alcañiz</p>
   </div></Link>
-</div>
+</div></div>
+<div className="row">
+<div className="col">
 <div className="card"><Link className="projectCard" to="../projectcard">
   <img src={project7} className="card-image7" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CASA SV13</h2>
     <p className="card-text">Private client - Internal reform - Menorca</p>
   </div></Link>
+</div></div></div>
 </div>
 </div>
 </>
