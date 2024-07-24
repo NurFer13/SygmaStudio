df86104abaa4b8ca8e19ff3f351eb97bed8decd9
NurFer13 <nurfer1307@gmail.com>
2024-07-24 10:31:29 +0000

    page1 done responsive

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function Page1 (){
     return(
-<>
-<NavBar/>
+        <>
 
+<NavBar/>
 <div className="page1">
     <ul><Link className="p1" to="/projects">PROJECTS</Link> </ul>
     <ul><Link className="p1" to="/about">OFFICE</Link> </ul>
