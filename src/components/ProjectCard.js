0b11628b6b05cd5cd95f717d655824a4ab09c4b9
NurFer13 <nurfer1307@gmail.com>
2024-08-28 17:56:22 +0000

    coming soon

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,7 @@
 import NavBar from "./NavBar";
-import project11 from "../Images/p1/1_a.png";
-import project12 from "../Images/p1/1_b.png";
-import project13 from "../Images/p1/1_c.png";
-import project14 from "../Images/p1/1_d.png";
-import project15 from "../Images/p1/1_e.png";
+import comingsoon from "../Images/comingsoon.png";
+
+
 
 import { Link } from "react-router-dom";
 
@@ -11,6 +9,7 @@ export default function projectCard(){
    return(
 <>
 <NavBar/>
+<img src={comingsoon} className="comingsoon" alt="..."/>
 
 </>
   );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -22,7 +22,10 @@ export default function Projects (){
   <img src={project1} className="card-image2" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">WHAT'S BEHIND</h2>
-    <p className="card-text">Competition Festival Model Temple – Barcelona</p>
+    <p className="card-text">Competition Festival Model</p>
+    <p className="card-text">  January 2023 </p>
+    <p className="card-text">Temple – Barcelona </p>
+
   </div></Link>
 </div>
 </div>
@@ -32,7 +35,11 @@ export default function Projects (){
   <img src={project2} className="card-image2" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">LAKE CHAPEL</h2>
-    <p className="card-text">Competition Reuse Italy - Pilgrim Ceter - San Giovani in Val di Lago</p>
+    <p className="card-text">Competition Reuse Italy</p>
+    <p className="card-text">February 2022</p>
+    <p className="card-text">Pilgrim Ceter - San Giovani in Val di Lago</p>
+
+
   </div></Link>
 </div>
 </div>
@@ -41,7 +48,9 @@ export default function Projects (){
   <img src={project3} className="card-image3" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">MUTUALISM</h2>
-    <p className="card-text">Competition Europan'17 - Civic building - Eibar</p>
+    <p className="card-text">Competition Europan'17</p>
+    <p className="card-text">June 2023</p>
+    <p className="card-text">Civic building - Eibar</p>
   </div></Link>
 </div>
 </div>
@@ -53,7 +62,10 @@ export default function Projects (){
   <img src={project4} className="card-image4" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CHIESA DIRUTA</h2>
-    <p className="card-text">Competition Reuse Italy -Concert Hall - Chiesa Diruta</p>
+    <p className="card-text">Competition Reuse Italy</p>
+    <p className="card-text">February 2021</p>
+    <p className="card-text">Concert Hall - Chiesa Diruta</p>
+
   </div></Link>
 </div></div>
 <div className="col">
@@ -61,7 +73,9 @@ export default function Projects (){
   <img src={project5} className="card-image5" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CASA GV506</h2>
-    <p className="card-text">Private client - Integral reform - Barcelona</p>
+    <p className="card-text">Private client</p>
+    <p className="card-text">September 2024</p>
+    <p className="card-text">Integral reform - Barcelona</p>
   </div></Link>
 </div></div>
 <div className="col">
@@ -69,7 +83,9 @@ export default function Projects (){
   <img src={project6} className="card-image6" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CENTRO FMG</h2>
-    <p className="card-text">Private client - New Construction - Alcañiz</p>
+    <p className="card-text">Private client</p>
+    <p className="card-text">In progress</p>
+    <p className="card-text">New Construction - Alcañiz</p>
   </div></Link>
 </div></div>
 </div>
@@ -80,7 +96,9 @@ export default function Projects (){
   <img src={project7} className="card-image7" alt="..."/>
   <div className="card-body">
     <h2 className="card-title">CASA SV13</h2>
-    <p className="card-text">Private client - Internal reform - Menorca</p>
+    <p className="card-text">Private client</p>
+    <p className="card-text">December 2023</p>
+    <p className="card-text">Internal reform - Menorca</p>
   </div></Link>
 </div></div></div>
 
